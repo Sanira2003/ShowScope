80e8a72be9a28e50e895a6d92900a2eb2187c848
Sanira Deneth <sanira.adesha@gmail.com>
2025-05-01 17:13:41 +0530

    changed folder structure.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return <>
+  <div className="home">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque labore, perferendis tenetur commodi quam incidunt voluptate laboriosam possimus dicta quidem asperiores, rerum quas vel ipsam. Fugit quis eum nisi odio unde molestias commodi quaerat in laborum earum, atque doloremque sed perferendis laudantium maxime ex voluptatem? Accusamus libero necessitatibus eveniet sit.</p>
+  </div>
+  </>;
+}
+
+export default App;
