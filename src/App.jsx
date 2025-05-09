e0d8cd4b7260a40168c9589fd17c6a7d000c81b8
Sanira Deneth <sanira.adehsa@gmail.com>
2025-05-09 11:17:40 +0530

    created navbar and added dark and light modes. defined color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./css/App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MovieCard from "./components/MovieCard"; 
-import movie from "./assets/movie.webp"
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
+import movie from "./assets/movie.webp";
 
 const router = createBrowserRouter([
   {
@@ -10,22 +15,23 @@ const router = createBrowserRouter([
       <MovieCard
         movie={{
           id: 1,
-          title: "sanira",
+          title: "Sanira Deneth Adesha",
           poster_path: movie,
           vote_avarage: "2",
           release_date: "2025",
         }}
       />
     ),
-  }
+  },
 ]);
-
-
 
 function App() {
   return (
     <>
-    <RouterProvider router={router} />
+      <BrowserRouter>
+        <Navbar></Navbar>
+      </BrowserRouter>
+      {/* <RouterProvider router={router} /> */}
     </>
   );
 }
