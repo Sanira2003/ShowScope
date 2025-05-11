feb79175a3e61178a3ce723e97f91be0a31b0d9c
Sanira Deneth <sanira.adesha@gmail.com>
2025-05-12 03:27:34 +0530

    implemented functionality to add to favorites and remove from favorites

--- src/components/FavoriteIcon.jsx
+++ src/components/FavoriteIcon.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { Heart } from "lucide-react";
 import "../css/FavoriteIcon.css";
+import { useUserdata } from "../contexts/UserdataContext";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-const FavoriteIcon = () => {
-  const [isFavorite, setIsFavorite] = useState(false);
+const FavoriteIcon = ({ movie }) => {
+  const { addToFavorites, removeFromFavorites, isFavoriteMovie } =
+    useUserdata();
+  const { currentUser } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleFavorite = () => {
+    if (!currentUser) {
+      navigate("/login");
+      return;
+    }
+    if (isFavoriteMovie(movie)) removeFromFavorites(movie);
+    else addToFavorites(movie);
+  };
 
   return (
-    <div
-      className="fav-btn-container"
-      onClick={() => setIsFavorite(!isFavorite)}
-    >
+    <div className="fav-btn-container" onClick={handleFavorite}>
       <Heart
-        color={isFavorite ? "#e50914" : "rgb(204, 204, 204, .8)"}
+        color={isFavoriteMovie(movie) ? "#e50914" : "rgb(204, 204, 204, .8)"}
         size={18}
-        fill={isFavorite ? "#e50914" : "transparent"}
+        fill={isFavoriteMovie(movie) ? "#e50914" : "transparent"}
       />
     </div>
   );
--- src/contexts/UserdataContext.jsx
+++ src/contexts/UserdataContext.jsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext, useContext, useEffect } from "react";
 import { db } from "../services/firebaseConfig";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { useAuth } from "./AuthContext";
 
 const UserdataContext = createContext();
@@ -10,48 +10,98 @@ export const useUserdata = () => useContext(UserdataContext);
 
 export const UserdataProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
+  const [favoriteMovies, setFavouriteMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useAuth();
 
-  const getUserRef = (user) => {
-    return doc(db, "users", user.uid);
-  };
+  const getUserRef = (user) => doc(db, "users", user.uid);
 
   const getUserDoc = async (user) => {
-    const userRef = getUserRef(user);
-    const docSnap = await getDoc(userRef);
-    return docSnap;
+    try {
+      const userRef = getUserRef(user);
+      return await getDoc(userRef);
+    } catch (err) {
+      console.error("Error fetching user doc:", err);
+      throw err;
+    }
   };
 
   const createUserDocumentIfNotExists = async (user) => {
-    const userRef = getUserRef(user);
-    const docSnap = await getUserDoc(user);
+    setIsLoading(true);
+    try {
+      const userRef = getUserRef(user);
+      const docSnap = await getUserDoc(user);
 
-    if (!docSnap.exists()) {
-      const newUserData = {
-        email: user.email,
-        name: user.displayName || "",
-        photoURL: user.photoURL || "",
-        country: "",
-        favoriteMovies: [],
-        favoriteMovie: "",
-        favoriteGenres: [],
-        bio: "",
-      };
-      await setDoc(userRef, newUserData);
-      setUserData(newUserData);
-      console.log("newdata");
-    } else setUserData(docSnap.data());
+      if (!docSnap.exists()) {
+        const newUserData = {
+          email: user.email,
+          name: user.displayName || "",
+          photoURL: user.photoURL || "",
+          country: "",
+          favoriteMovies: [],
+          favoriteMovie: "",
+          favoriteGenres: [],
+          bio: "",
+        };
+        await setDoc(userRef, newUserData);
+        setUserData(newUserData);
+      } else {
+        setUserData(docSnap.data());
+      }
+    } catch (err) {
+      console.error("Error creating user document:", err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const removeUserdata = () => {
-    setUserData({});
+    setUserData(null);
   };
+
+  const addToFavorites = (movie) => {
+    setFavouriteMovies((prev) => {
+      const exists = prev.some((m) => m.id === movie.id);
+      return exists ? prev : [...prev, movie];
+    });
+  };
+
+  const removeFromFavorites = (movie) => {
+    const updateMovies = favoriteMovies.filter((m) => m.id !== movie.id);
+    setFavouriteMovies(updateMovies);
+  };
+
+  const isFavoriteMovie = (movie) => {
+    return favoriteMovies.some((m) => m.id === movie.id);
+  };
+
+  useEffect(() => {
+    const updateFavoriteMovies = async () => {
+      if (!currentUser) return;
+      try {
+        const userRef = doc(db, "users", currentUser.uid);
+        await updateDoc(userRef, { favoriteMovies: favoriteMovies });
+        console.log("Updated Firestore successfully!");
+      } catch (error) {
+        console.error("Failed to update Firestore:", error);
+      }
+    };
+    updateFavoriteMovies();
+  }, [favoriteMovies, currentUser]);
 
   useEffect(() => {
     const initializeUserData = async () => {
       if (currentUser) {
-        const docSnap = await getUserDoc(currentUser);
-        setUserData(docSnap.data());
+        setIsLoading(true);
+        try {
+          const docSnap = await getUserDoc(currentUser);
+          setUserData(docSnap.data());
+          setFavouriteMovies(userData.favoriteMovies);
+        } catch (err) {
+          console.error("Error initializing user data:", err);
+        } finally {
+          setIsLoading(false);
+        }
       }
     };
     initializeUserData();
@@ -61,11 +111,15 @@ export const UserdataProvider = ({ children }) => {
     createUserDocumentIfNotExists,
     userData,
     removeUserdata,
+    favoriteMovies,
+    addToFavorites,
+    removeFromFavorites,
+    isFavoriteMovie,
   };
 
   return (
     <UserdataContext.Provider value={value}>
-      {children}
+      {!isLoading && children}
     </UserdataContext.Provider>
   );
 };
