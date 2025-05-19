b85f9a00c510b852185695e5cef94449a7ecf2fd
Sanira Deneth <sanira.adehsa@gmail.com>
2025-05-20 03:07:44 +0530

    fixed scrolling error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import "./css/App.css";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-import Favourite from "./pages/Favourite";
+import Favorite from "./pages/Favourite";
 import { UserdataProvider } from "./contexts/UserdataContext";
 import EditProfile from "./pages/EditProfile";
 import Search from "./pages/Search";
 import Movie from "./pages/Movie";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  const location = useLocation();
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <UserdataProvider>
-          <Navbar />
-          <Routes location={location} key={location.key}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/favorite" element={<Favourite />} />
-            <Route path="/editProfile" element={<EditProfile />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/movies/:id" element={<Movie />} />
-          </Routes>
-        </UserdataProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <UserdataProvider>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/editProfile" element={<EditProfile />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movies/:id" element={<Movie />} />
+        </Routes>
+      </UserdataProvider>
+    </AuthProvider>
   );
 }
 
